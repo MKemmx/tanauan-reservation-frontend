cca0cbea62ea3a1f7441f0d618f922dfdbb598fe
Kim <kimasdilla@gmail.com>
2022-11-12 08:43:32 +0800

    new update

--- src/AdminScreen/Dashboard/Dashboard.jsx
+++ src/AdminScreen/Dashboard/Dashboard.jsx
@@ -39,7 +39,7 @@ const Dashboard = () => {
 
   return (
     <>
-      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
+      <div className="p-4 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4">
         <div
           onClick={() => {
             navigate("/reservation");

796532a0ce1f17fd1021106cf8173455d45dbd9f
Kim <kimasdilla@gmail.com>
2022-11-28 23:07:49 +0800

    asd

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
-// const api = "http://localhost:5000/api";
+const api = "http://localhost:5000/api";
 
-const api = "https://reservation-system-back-end.herokuapp.com/api";
+// const api = "https://reservation-system-back-end.herokuapp.com/api";
 
 export default api;
